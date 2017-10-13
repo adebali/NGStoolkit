8ac3512bf4414e90fff2e5a023ef512237ea06d6
Adebali <adebali@users.noreply.github.com>
2017-10-13 10:10:44 -0400

    reorganization of the scripts

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "EXECUTABLE_SCRIPTS=\$HOME/sancarlabutils
+echo "EXECUTABLE_SCRIPTS=\$HOME/sancarlabutils/bin
 PATH=\$PATH:\$EXECUTABLE_SCRIPTS
 export PATH
 CUSTOM_PYTHON_SCRIPTS=\$EXECUTABLE_SCRIPTS/utils

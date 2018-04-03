3036a4c96097b5681ed3501ff44eea5ad5760d58
Adebali <adebali@users.noreply.github.com>
2018-04-02 21:07:31 -0400

    modern IO

--- bin/bed2binnedBed.py
+++ bin/bed2binnedBed.py
@@ -3,8 +3,8 @@
 import argparse
 
 PARSER = argparse.ArgumentParser(description='converts bed file into smaller bins')
-PARSER.add_argument('-i', required=True, help='input')
-PARSER.add_argument('-o', required=True, help='output')
+PARSER.add_argument('-i', nargs='?', type=argparse.FileType('r'), default=sys.stdin, help='input')
+PARSER.add_argument('-o', nargs='?', type=argparse.FileType('w'), default=sys.stdout, help='output')
 PARSER.add_argument('-s', required=True, help='bin size')
 PARSER.add_argument('-n', required=True, help='bin number')
 PARSER.add_argument('--noShort', required= False, action='store_true', help='ignore flanking regions shorter than bin size')
@@ -14,8 +14,8 @@ BED_FILE = ARGS.i
 OUT_BED_FILE = ARGS.o
 BIN_SIZE = int(ARGS.s)
 BIN_NUMBER = int(ARGS.n)
-FILE_IN = open(BED_FILE, 'r')
-OUT = open(OUT_BED_FILE, 'w')
+FILE_IN = args.i
+OUT = args.o
 NO_SHORT_FLAG = ARGS.noShort
 INTERVAL_NUMBER = 0
 
